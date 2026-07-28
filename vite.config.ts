befbce1deb0aaaf2481ec06f1128a87386ca0ca0
tinywolf <tinywolf00@gmail.com>
2026-07-28 19:30:40 +0900

    GitHub Pages 자동 배포 구성 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/roulette/",
   plugins: [react()],
   server: {
     allowedHosts: [".dev.daum.net"],
